dae8667045b3dfee1794fbfc66ee0e5b5534e9d8
ᴇʀʏxɪ <bigsamaa@gmail.com>
2024-01-29 05:12:45 +0700

    :wrench: add breadcrumb key

--- src/components/ui/Breadcrumb.jsx
+++ src/components/ui/Breadcrumb.jsx
@@ -5,10 +5,10 @@ const Breadcrumb = ({ children }) => {
     return (
         <ul className="flex items-center gap-2 text-lg">
             {children.map((child, idx) => (
-                <>
+                <React.Fragment key={`breadcrum-${idx}`}>
                     <li className={`${idx == children.length -1 ? "text-zinc-600" : "text-blue-600"}`}>{child}</li>
                     {idx < children.length -1 && <li className="text-zinc-600">/</li>}
-                </>
+                </React.Fragment>
             ))}
         </ul>
     )
