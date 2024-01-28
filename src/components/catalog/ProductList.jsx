7a00f66caaf68d9431d065b929de5c247244738e
ᴇʀʏxɪ <bigsamaa@gmail.com>
2024-01-29 05:14:05 +0700

    :lipstick: change cart button color

--- src/components/catalog/ProductList.jsx
+++ src/components/catalog/ProductList.jsx
@@ -3,14 +3,14 @@ import ProductCard from "./ProductCard"
 
 const ProductList = () => {
     return (
-        <div className='w-full flex flex-col p-4 shadow-md rounded-md'>
-            <div className='w-full flex items-center justify-between'>
+        <div className='w-full flex flex-col gap-2 p-4 shadow rounded-md'>
+            <div className='w-full flex items-center justify-between py-1'>
                 <span className="text-lg">Product List</span>
-                <button type='button' class='bg-blue-600 hover:bg-blue-600 text-white transition-all duration-200 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 flex items-center gap-2 self-center'>
-                    Reset
+                <button type='button' className='bg-yellow-500 hover:bg-yellow-400 text-white transition-all duration-200 focus:outline-none rounded-lg text-sm px-5 py-2.5 text-center flex items-center gap-2 self-center'>
+                    Cart
                 </button>
             </div>
-            <div className="grid grid-cols-4 gap-6">
+            <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                 <ProductCard />
                 <ProductCard />
                 <ProductCard />
--- src/components/layout/Topbar.jsx
+++ src/components/layout/Topbar.jsx
@@ -6,16 +6,16 @@ import { FaRegBell, FaRegEnvelope, FaRegMoon } from "react-icons/fa6"
 
 const Topbar = () => {
     return (
-        <div className='w-full flex items-center justify-between bg-[#fff] shadow-md rounded-lg p-3'>
+        <div className='w-full flex items-center justify-between bg-[#fff] shadow rounded-lg p-3'>
             <SearchBox />
             <div className='flex items-center justify-end flex-row-reverse gap-3'>
                 <div className='w-10 h-10 bg-zinc-100/50 rounded-full overflow-hidden flex items-center justify-center'>
-                    <Image src={"/assets/images/mockup.jpg"} width={64} height={64} alt="Avatar" />
+                    <Image src={"/assets/images/mockup.jpg"} width={64} height={64} alt='Avatar' />
                 </div>
                 <IconButton notify icon={<FaRegBell />} />
                 <IconButton icon={<FaRegEnvelope />} />
-                <IconButton icon={<Image src={"/assets/images/us.png"} width={24} height={24} alt="Language" />} />
-                <IconButton icon={<FaRegMoon/>} />
+                <IconButton icon={<Image src={"/assets/images/us.png"} width={24} height={24} alt='Language' />} />
+                <IconButton icon={<FaRegMoon />} />
             </div>
         </div>
     )
