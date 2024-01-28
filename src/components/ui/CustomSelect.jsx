668575ac17696b1eadf259a51372be1ec4ce4ef6
ᴇʀʏxɪ <bigsamaa@gmail.com>
2024-01-29 05:12:27 +0700

    :wrench: add placeholder to select

--- src/components/ui/CustomSelect.jsx
+++ src/components/ui/CustomSelect.jsx
@@ -3,8 +3,8 @@ import React from "react"
 const CustomSelect = ({ className }) => {
     return (
         /* Ref: https://flowbite.com/docs/forms/select/ */
-        <select class={`${className} border border-zinc-200 text-zinc-600 rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 `}>
-            <option selected>Ex : หลอด , ชุด , ขวด</option>
+        <select defaultValue={"placeholder"} className={`${className} border border-zinc-200 text-zinc-600 rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 `}>
+            <option value='placeholder' disabled hidden>Ex : หลอด , ชุด , ขวด</option>
             <option value='1'>Item 1</option>
             <option value='2'>Item 2</option>
             <option value='3'>Item 3</option>
